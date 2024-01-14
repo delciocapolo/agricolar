dca9455876c96ba9879c930df212ff83b39faaf5
Delcio Capolo <delciocapolo096@gmail.com>
2024-01-14 15:46:33 +0100

    components and style (mounted setup)

---
+++ src/DTO/service.ts
@@ -0,0 +1,25 @@
+import { IService } from "../components/skeleton/Service";
+import BackgroundImage from "../assets/logo.svg";
+
+export const services: IService[] = [
+  {
+    url: "logistica",
+    pathImage: BackgroundImage,
+    descriptionImg: "elaborar estrategias e consultoria",
+  },
+  {
+    url: "marketing",
+    pathImage: BackgroundImage,
+    descriptionImg: "consolidar fazenda no mercado digital",
+  },
+  {
+    url: "agricolar",
+    pathImage: BackgroundImage,
+    descriptionImg: "monitoramento áereo das plantações",
+  },
+  {
+    url: "farmonline",
+    pathImage: BackgroundImage,
+    descriptionImg: "tornar a fazenda online",
+  },
+];
