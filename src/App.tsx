2ea1cdfdee9fa79deb95c7473a2d2644f30f9385
Delcio Capolo <delciocapolo096@gmail.com>
2024-01-18 15:19:11 +0100

    new palete and configs. defined

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
 import {JSX} from "react";
-import Header from "./components/header/Header";
-import Skeleton from "./components/skeleton/Skeleton";
 
 const App = ():JSX.Element => {
   return (
     <main className="main">
-      <Header />
-      <Skeleton />
     </main>
   );
 }
