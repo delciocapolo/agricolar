21398e3ba333ab6e8ebdd34705fc38ce0b865f33
Delcio Capolo <delciocapolo096@gmail.com>
2024-02-12 18:25:11 +0100

    working at 'progress tracker point'

---
+++ src/components/main-components/ProgressTrackerPoint/Default.tsx
@@ -0,0 +1,9 @@
+const Default = () => {
+    return (
+        <div className="container-progress-tracker-point">
+            <span className="title-progress-tracker-point">2</span>
+        </div>
+    )
+}
+
+export default Default;
