8bc780e71ea5a36a5e6aaf9c9b4b4ad34ecf79ac
Delcio Capolo <delciocapolo096@gmail.com>
2024-02-16 17:34:00 +0100

    working at progress-tracker...

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
-import ProgressTrackerPoint from "./components/main-components/ProgressTrackerPoint/ProgressTrackerPoint";
+import ProgressTrackerPoint, { IProgressTrackerPoint } from "./components/main-components/ProgressTrackerPoint/ProgressTrackerPoint";
 
 const App = () => {
+  const tracker: Array<IProgressTrackerPoint> = [
+    {
+      statusPoint: 'initial-point'
+    },
+    {
+      statusPoint: 'initial-point'
+    },
+    {
+      statusPoint: 'initial-point'
+    },
+    {
+      statusPoint: 'initial-point'
+    },
+    {
+      statusPoint: 'initial-point'
+    },
+    {
+      statusPoint: 'initial-point'
+    },
+  ]
   return (
     <main className="main">
       <div className="aux d-flex">
-        <ProgressTrackerPoint />
+        <ProgressTrackerPoint tracker={tracker} />
       </div>
     </main>
   );
--- src/components/main-components/ProgressTrackerPoint/ProgressTrackerPoint.tsx
+++ src/components/main-components/ProgressTrackerPoint/ProgressTrackerPoint.tsx
@@ -1,58 +1,86 @@
 import { Check } from "lucide-react";
-import { FC, useState, SetStateAction, Dispatch } from "react";
+import { FC, useEffect, useState } from "react";
+import {v4 as uuid} from 'uuid';
 
-
-
-interface IProgressTrackerPoint {
-    numberSteps: number;
-    statusPoint: string;
+export interface IProgressTrackerPoint {
+    numberSteps?: number | string;
+    statusPoint: statusPoint;
 }
 
-type statusPoint = 'initial-point' | 'mid-point' | 'done-point'
-type progressive = boolean;
+type statusPoint = 'initial-point' | 'mid-point' | 'done-point';
 
-const changeProgressPoint = (status: statusPoint = 'initial-point', progressive: progressive = true ,setState: Dispatch<SetStateAction<statusPoint>>) => {
+interface II {
+    tracker: Array<IProgressTrackerPoint>
+}
+
+const changeProgressPoint = (status: statusPoint = 'initial-point', progressive:boolean = true, setState: IProgressTrackerPoint) => {
     if(status === 'initial-point' && progressive) {
-        setState('mid-point');
+        setState.statusPoint = 'mid-point';
         return;
     }
     if(status === 'mid-point' && progressive) {
-        setState('done-point');
+        setState.statusPoint = 'done-point';
         return
     }
     
     if(status === 'done-point' && !progressive) {
-        setState('mid-point');
+        setState.statusPoint = 'mid-point';
         return;
     }
     if(status === 'mid-point' && !progressive) {
-        setState('initial-point');
+        setState.statusPoint = 'initial-point';
         return
     }
 }
 
-const ProgressTrackerPoint = () => {
-    const size = 15;
+const ProgressTrackerPoint:FC<II> = ({tracker}) => {
+    const [stepNumber, setStepNumber] = useState<number>(0);
+    const size = 23;
+
+    useEffect(() => {
+        if(stepNumber < 0) {
+            setStepNumber(step => step + 1);
+        }
+        if(stepNumber > tracker.length) {
+            setStepNumber(step => step - 1);
+        }
+    }, [stepNumber]);
 
     return (
-        <div className="container-progress-tracker-point d-flex">
-            {/* {
-                IProgressPoint.map(({numberSteps}) => )
-            } */}
-            <nav className="container-tracker d-flex">
-                <div className="container-progress-point d-flex">
-                    <span className="progress-point initial-point d-flex">1</span>
-                    <div className="line-progress-tracker"></div>
-                </div>
-                <div className="container-progress-point d-flex">
-                    <span className="progress-point mid-point d-flex">2</span>
-                    <div className="line-progress-tracker"></div>
-                </div>
-            </nav>
-            <span className="progress-point done-point d-flex">
-                <Check size={size} style={{color: 'var(--White)'}} />
-            </span>
-        </div>
+        <>
+            <div className="container-progress-tracker-point d-flex">
+                <ProgressTrackerPoints tracker={tracker} size={size}/>
+            </div>
+            <div className="d-flex">
+                <button type="button" onClick={() => {
+                    setStepNumber(step => step + 1);
+                    console.log()
+                    // changeProgressPoint(tracker[stepNumber].statusPoint, true, tracker[stepNumber]);
+                }}>Avancar</button>
+                <button type="button" onClick={() => {
+                    setStepNumber(step => step - 1);
+                    // changeProgressPoint(tracker[stepNumber].statusPoint, false, tracker[stepNumber]);
+                }}>Recuar</button>
+            </div>
+        </>
+    )
+}
+
+const ProgressTrackerPoints:FC<II & {size: number}> = ({tracker, size=20}) => {
+    return (
+        <nav className="container-tracker d-flex">
+            {
+                tracker.map(({statusPoint}, index) => (
+                    <div className="container-progress-point d-flex" key={uuid()}>
+                        <span className={`progress-point ${statusPoint} d-flex`}>
+                            { statusPoint === 'done-point' && <Check size={size} style={{color: 'var(--White)'}} absoluteStrokeWidth={true} /> }
+                            { statusPoint !== 'done-point' && (index+1) }
+                        </span>
+                        { ((index+1) !== tracker.length) && <div className="line-progress-tracker"></div> }
+                    </div>
+                ))
+            }
+        </nav>
     )
 }
 
