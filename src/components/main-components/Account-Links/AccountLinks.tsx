ce08413f6011701c79a47c20eca37258e8c5e233
Delcio Capolo <delciocapolo096@gmail.com>
2024-02-23 21:43:05 +0100

    account-links done

--- src/components/main-components/Account-Links/AccountLinks.tsx
+++ src/components/main-components/Account-Links/AccountLinks.tsx
@@ -1,18 +1,25 @@
 import { LayoutDashboard } from "lucide-react";
 import { FC } from "react";
 
-const AccountLinks:FC = () => {
-    const size = 24;
-    return (
-        <a className="container-account-links d-flex" href="http://" target="_blank" rel="noopener noreferrer">
-            <div className="container-image-account-link d-flex">
-                <LayoutDashboard size={size} style={{fill: 'var(--Gray-900)'}} />
-            </div>
-            <div className="container-title-account-link d-flex">
-                <h1 className="title-account-link">Dashboard</h1>
-            </div>
-        </a>
-    )
-}
+const AccountLinks: FC = () => {
+  const size = 25;
+  return (
+    <a
+      className="container-account-links d-flex"
+      href="http://"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <div className="container-image-account-link d-flex">
+        <LayoutDashboard
+          size={size}
+          style={{ fill: "var(--Gray-900)", color: "var(--Gray-600)" }}
+          className="image-account-link"
+        />
+      </div>
+      <h1 className="title-account-link">Dashboard</h1>
+    </a>
+  );
+};
 
 export default AccountLinks;
