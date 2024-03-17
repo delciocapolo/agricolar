5f6a3bdba10c5c2da287d26152a5f5aa50c6d73c
delciocapolo <delciocapolo096@gmail.com>
2024-03-17 09:44:04 +0100

    working at footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,17 @@
+import styled from "@emotion/styled";
+import { Container as ContainerBase } from "../Container/Container";
+
+
+const Footer = () => {
+    const FooterContainer = styled['div']``;
+    const Container = styled(ContainerBase)``;
+    return (
+        <FooterContainer>
+            <Container>
+
+            </Container>
+        </FooterContainer>
+    )
+};
+
+export default Footer;
