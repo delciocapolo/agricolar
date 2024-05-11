0c1732fc238901f9a2c34b678c5d3bd5f749ec7e
delciocapolo <delciocapolo096@gmail.com>
2024-05-11 22:49:00 +0100

    trying improve performance and moved directory(SignComponent)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,44 @@
 import { useEffect, useState, lazy, Suspense } from "react";
+import i10 from './assets/header/HeaderPhoto2.jpg';
+import i19 from './assets/newsletterpopup/newsletterpopup.jpg';
+import i20 from './assets/products/Image.jpg';
+import { cacheImages } from "./components/utils/CacheImages";
+import Spinner from "./components/main-components/Spinner/Spinner";
+import ContextComponent from "./components/ContextComponent/ContextComponent";
+
+const Menu = lazy(() => import("./components/Navigation/Menu"));
+const Skeleton = lazy(() => import("./components/Body/Skeleton"));
+const Footer = lazy(() => import("./components/Footer/Footer"));
+const Header = lazy(() => import("./components/Header/Header"));
+
+const App = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    // const imgs = [i10, i19, i20];
+    // cacheImages(imgs, setIsLoading);
+    setIsLoading(prev => (!prev));
+  }, []);
+
+  return (
+    <ContextComponent>
+      <Suspense fallback={<Spinner loading={isLoading} />}>
+        <Menu />
+        <Header />
+        <Skeleton />
+        <Footer />
+      </Suspense>
+    </ContextComponent>
+  );
+};
+
+export default App;
+
+
+
+
+
+
 
 // import i4 from './assets/blogcard/card1.jpg';
 // import i5 from './assets/blogcard/card2.jpg';
@@ -11,39 +51,3 @@ import { useEffect, useState, lazy, Suspense } from "react";
 // import i16 from './assets/instagram/i6.jpg';
 // import i17 from './assets/instagram/i7.jpg';
 // import i18 from './assets/instagram/i8.jpg';
-import i10 from './assets/header/HeaderPhoto2.jpg';
-import i19 from './assets/newsletterpopup/newsletterpopup.jpg';
-import i20 from './assets/products/Image.jpg';
-import { cacheImages } from "./components/utils/CacheImages";
-import Spinner from "./components/main-components/Spinner/Spinner";
-import SignComponent from "./components/Sign/Sign";
-import ContextComponent from "./components/ContextComponent/ContextComponent";
-
-const Menu = lazy(() => import("./components/Navigation/Menu"));
-const Skeleton = lazy(() => import("./components/Body/Skeleton"));
-const Footer = lazy(() => import("./components/Footer/Footer"));
-const Header = lazy(() => import("./components/Header/Header"));
-
-const App = () => {
-  const [isLoading, setIsLoading] = useState<boolean>(true);
-
-  useEffect(() => {
-    const imgs = [i10, i19, i20];
-
-    cacheImages(imgs, setIsLoading);
-  }, []);
-
-  return (
-    <ContextComponent>
-      <Suspense fallback={<Spinner loading={isLoading} />}>
-        <SignComponent />
-        <Menu />
-        <Header />
-        <Skeleton />
-        <Footer />
-      </Suspense>
-    </ContextComponent>
-  );
-};
-
-export default App;
